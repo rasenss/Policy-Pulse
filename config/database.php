ed29c14aef2787509f865a1ec5ddbdf811db85b2
rasendkhansa <rasuen27@gmail.com>
2025-11-20 20:34:21 +0700

    Fix supabase transaction pooler config

--- config/database.php
+++ config/database.php
@@ -43,6 +43,26 @@ return [
             'transaction_mode' => 'DEFERRED',
         ],
 
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
+            
+            // TAMBAHKAN BAGIAN INI AGAR SUPABASE 6543 JALAN:
+            'attributes' => [
+                PDO::ATTR_EMULATE_PREPARES => true,
+            ],
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
